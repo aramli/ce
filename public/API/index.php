96d7c69305605b99a3fe31718b9688f9cb1fe1ed
Hebert Hendrik <hebert.hendrik@gmail.com>
2020-10-14 18:11:23 +0700

    upcoming event

--- public/API/index.php
+++ public/API/index.php
@@ -45,6 +45,60 @@ if( $_POST['module'] == "GetUserRecentEvent" ){
 	
 }
 
+if( $_POST['module'] == "GetAllUpcomingEvent" ){
+
+	$query = "select * from d3s3m_event where eve_STATUS = 4 and eve_EVENT_START >= '".date('Y-m-d H:i:s')."' order by eve_EVENT_START DESC ";
+	$result = $db->query($query);
+	$num = $result->num_rows;
+	
+	$json['DISPLAY_UPCOMING_EVENT'] = '';
+
+	if( $num > 0 ){
+		
+		for( $i=0;$i<$num;$i++ ){
+			
+			$row = $result->fetch_assoc();
+			
+			$json['DISPLAY_UPCOMING_EVENT'] .= '
+
+			<!--begin::Item-->
+			<div class="d-flex align-items-center mb-10">
+				<!--begin::Symbol-->
+				<div class="symbol symbol-40 symbol-light-primary mr-5">
+					<span class="symbol-label">
+						<span class="svg-icon svg-icon-lg svg-icon-primary">
+							<!--begin::Svg Icon | path:assets/media/svg/icons/Home/Library.svg-->
+							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+								<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+									<rect x="0" y="0" width="24" height="24" />
+									<path d="M5,3 L6,3 C6.55228475,3 7,3.44771525 7,4 L7,20 C7,20.5522847 6.55228475,21 6,21 L5,21 C4.44771525,21 4,20.5522847 4,20 L4,4 C4,3.44771525 4.44771525,3 5,3 Z M10,3 L11,3 C11.5522847,3 12,3.44771525 12,4 L12,20 C12,20.5522847 11.5522847,21 11,21 L10,21 C9.44771525,21 9,20.5522847 9,20 L9,4 C9,3.44771525 9.44771525,3 10,3 Z" fill="#000000" />
+									<rect fill="#000000" opacity="0.3" transform="translate(17.825568, 11.945519) rotate(-19.000000) translate(-17.825568, -11.945519)" x="16.3255682" y="2.94551858" width="3" height="18" rx="1" />
+								</g>
+							</svg>
+							<!--end::Svg Icon-->
+						</span>
+					</span>
+				</div>
+				<!--end::Symbol-->
+				<!--begin::Text-->
+				<div class="d-flex flex-column font-weight-bold">
+					<a  class="text-dark text-hover-primary mb-1 font-size-lg">'.$row['eve_TITLE'].'</a>
+					<span class="text-muted">'.$row['eve_EVENT_START'].'</span>
+				</div>
+				<!--end::Text-->
+			</div>
+			<!--end::Item-->
+			';
+		}
+	} else {
+		$json['DISPLAY_UPCOMING_EVENT'] = 'There are no upcoming events.';
+	}
+
+
+	echo json_encode($json);
+
+}
+
 
 
 ?>
